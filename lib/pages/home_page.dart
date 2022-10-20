ae92044d39432e0fa0a94f67d8a41d00693e9e86
asadbek-ip <dasturchi6678@gmail.com>
2022-10-20 09:18:54 +0500

    finish

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,7 +16,7 @@ class _HomePageState extends State<HomePage> {
         elevation: 0,
         title: Text(
           "Cars",
-          style: TextStyle(color: Colors.black),
+          style: TextStyle(color: Colors.red),
         ),
         backgroundColor: Colors.white,
         actions: [
@@ -24,13 +24,13 @@ class _HomePageState extends State<HomePage> {
               onPressed: () {},
               icon: Icon(
                 Icons.notifications_none,
-                color: Colors.black,
+                color: Colors.red,
               )),
           IconButton(
               onPressed: () {},
               icon: Icon(
                 Icons.shopping_cart_rounded,
-                color: Colors.black,
+                color: Colors.red,
               ))
         ],
       ),
@@ -61,9 +61,9 @@ class _HomePageState extends State<HomePage> {
                       "Ferrari", "1300\$", "Electric", "assets/images/car.jpg"),
                   _cardItem(
                       "Ferrari", "1300\$", "Electric", "assets/images/car.jpg"),
-                      _cardItem(
+                  _cardItem(
                       "Ferrari", "1300\$", "Electric", "assets/images/car.jpg"),
-                      _cardItem(
+                  _cardItem(
                       "Ferrari", "1300\$", "Electric", "assets/images/car.jpg"),
                 ],
               ),
@@ -81,12 +81,12 @@ Widget _categoriyaItem(bool isActive, String name) {
     child: Container(
       margin: EdgeInsets.only(left: 10),
       decoration: BoxDecoration(
-          color: isActive ? Colors.grey.shade300 : Colors.white,
+          color: isActive ? Colors.red : Colors.white,
           borderRadius: BorderRadius.all(Radius.circular(10))),
       child: Center(
         child: Text(
           name,
-          style: TextStyle(color: Colors.black),
+          style: TextStyle(color: isActive ? Colors.white : Colors.black),
         ),
       ),
     ),
@@ -100,42 +100,65 @@ Widget _cardItem(String name, String price, String tipe, String img) {
     ),
     child: Container(
       width: double.infinity,
-      height: 300,
+      height: 250,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
         image: DecorationImage(fit: BoxFit.cover, image: AssetImage(img)),
       ),
       child: Container(
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.black45,
-                  Colors.black38,
-                  Colors.black26,
-                ]),),
-                child: Container(
-                  margin: EdgeInsets.all(10),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          Text(name,style:  TextStyle(color: Colors.white,fontSize: 16),),
-                          SizedBox(height: 10,),
-                          Text(tipe,style:  TextStyle(color: Colors.white,fontSize: 14),),
-                           Spacer(),
-                          Text(price,style:  TextStyle(color: Colors.white,fontSize: 16),),
-
-                        ],
-                      ),
-                      Icon(Icons.favorite_border,size: 40,color: Colors.white,)
-                    ],
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.black45,
+                Colors.black38,
+                Colors.black26,
+              ]),
+        ),
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.w600),
                   ),
-                ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    tipe,
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 25,
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                price,
+                style: TextStyle(color: Colors.white, fontSize: 30,fontWeight: FontWeight.w500),
+              ),
+              Spacer(),
+               CircleAvatar(
+                backgroundColor: Colors.red,
+                 child: Icon(
+                  Icons.favorite_border,
+                  size: 35,
+                  color: Colors.white,
+                             ),
+               )
+            ],
+          ),
+        ),
       ),
     ),
   );
